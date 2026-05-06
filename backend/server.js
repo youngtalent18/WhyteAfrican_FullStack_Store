fcba72c5060d76d4a8397a4b1a621645abb3f4c6
Stephen Anti <stephenanti63@gmail.com>
2026-05-06 05:36:49 -0700

    1-routing fix

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: [
+        "http://localhost:5173",
+        process.env.CLIENT_URL
+    ],
     credentials: true,
 }));
 app.use(cookieParser());
