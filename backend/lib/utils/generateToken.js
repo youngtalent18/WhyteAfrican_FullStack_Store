bf091f14c0fad088fbb576771c567b0bd3145ba4
Stephen Anti <stephenanti63@gmail.com>
2026-05-06 05:31:23 -0700

    1-cookie fix

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -11,14 +11,14 @@ export const generateTokens = async(userId) =>{
 
 export const setCookies = (res, accessToken, refreshToken) => {
     res.cookie("accessToken", accessToken, {
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        sameSite: "none",
         httpOnly: true,
         secure: process.env.NODE_ENV !== "development",
         maxAge: 15*60*1000
     });
 
     res.cookie("refresh", refreshToken, {
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        sameSite: "none",
         httpOnly: true,
         secure: process.env.NODE_ENV !== "development",
         maxAge: 7*24*60*60*1000
