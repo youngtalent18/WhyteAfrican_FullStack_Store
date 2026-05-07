476603ed9b7c418c82aeb5f8b68cb14e74af5723
Stephen Anti <stephenanti63@gmail.com>
2026-05-07 14:03:17 -0700

    1-fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-  timeout: 20000, // 🔥 20 seconds
 });
 
 export default api;
