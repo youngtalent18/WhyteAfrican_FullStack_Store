4628e4eefd0f7ea5b52d43c32ff7b7a860ecc3e7
Stephen Anti <stephenanti63@gmail.com>
2026-05-14 06:23:23 -0700

    1-server logic update

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use(
   cors({
     origin: [
       "https://whyte-african-full-stack-store.vercel.app",
+      "http://localhost:5173",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
