c753f0ce7bbd60182be99115a09dcfddc69210e6
Stephen Anti <stephenanti63@gmail.com>
2026-05-14 00:54:45 -0700

    1-added method patch

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(
       "https://whyte-african-full-stack-store.vercel.app",
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
