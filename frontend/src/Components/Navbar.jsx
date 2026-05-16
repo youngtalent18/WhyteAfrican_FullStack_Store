6f598efc73439420b04ca5ab5b1950866a15bda2
Stephen Anti <stephenanti63@gmail.com>
2026-05-16 04:19:58 -0700

    1-fix

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -248,7 +248,7 @@ const Navbar = ({
                     logout();
                     setMenuOpen(false);
                   }}
-                  className="w-full border border-red-400/90 bg-red-200/70 hover:bg-red-500/90 py-2 rounded-lg text-red-400/90 flex items-center justify-center gap-1"
+                  className="w-full border border-red-500/90 bg-red-400/90 hover:bg-red-500 py-2 rounded-lg text-red-500/90 flex items-center justify-center gap-1"
                 >
                   <LogOut size={15} />
                   Logout
